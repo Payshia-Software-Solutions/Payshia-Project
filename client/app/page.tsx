442ba24b588de74c6bb3b582d8576e7824cc6547
Thilina Ruwan <106557565+thilinaruwan112@users.noreply.github.com>
2024-08-15 22:43:02 +0530

    update-hero

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import Cta from "@/components/CTA/Cta";
+import Hero from "@/components/Hero/Hero";
 
 export default function Home() {
   return (
     <div>
+      <Hero />
       <Cta />
     </div>
   );
---
+++ client/components/Hero/Hero.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <section
+      className="bg-gray-50 bg-cover bg-center"
+      style={{
+        backgroundImage:
+          'url("https://plus.unsplash.com/premium_photo-1668473366551-a6c721292360?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
+      }}
+    >
+      <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
+        <div className="mx-auto max-w-2xl text-center bg-white bg-opacity-40 p-6 rounded-lg">
+          <h1 className="text-3xl font-extrabold sm:text-5xl">
+            Understand User Flow.
+            <strong className="font-extrabold text-red-700 sm:block">
+              {" "}
+              Increase Conversion.{" "}
+            </strong>
+          </h1>
+
+          <p className="mt-4 sm:text-xl/relaxed">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt
+            illo tenetur fuga ducimus numquam ea!
+          </p>
+
+          <div className="mt-8 flex flex-wrap justify-center gap-4">
+            <a
+              className="block w-full rounded bg-red-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
+              href="#"
+            >
+              Get Started
+            </a>
+
+            <a
+              className="block w-full rounded px-12 py-3 text-sm font-medium text-red-600 shadow hover:text-red-700 focus:outline-none focus:ring active:text-red-500 sm:w-auto"
+              href="#"
+            >
+              Learn More
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
